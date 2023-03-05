8627a4f31fcafb2fea46a46463b8d6d41350120a
Bastien <45488913+BastienBiblocque@users.noreply.github.com>
2023-03-05 20:52:20 +0100

    v 1.1 pour upload

--- index.js
+++ index.js
@@ -24,13 +24,25 @@ client.on('ready', () => {
 // Événement de réception de message
 client.on('messageCreate', (message) => {
     console.log(message.content)
+    const quoiValue = ['quoi', 'Quoi', 'quoi ?', 'Quoi ?'];
+    const heinValue = ['hein', 'Hein', 'hein ?', 'Hein ?'];
+    const random = Math.random();
+    console.log(random)
     if (message.content.startsWith(prefixLong) && !message.author.bot) {
-        sendSpotifyLinkWithYoutubeId(message, message.content.slice(prefixLong.length))
+        // sendSpotifyLinkWithYoutubeId(message, message.content.slice(prefixLong.length))
     }
     else if (message.content.startsWith(prefixShort) && !message.author.bot) {
-        sendSpotifyLinkWithYoutubeId(message, message.content.slice(prefixShort.length))
-    } else if (message.content === 'quoi' && !message.author.bot) {
+        // sendSpotifyLinkWithYoutubeId(message, message.content.slice(prefixShort.length))
+    }
+    else if ((quoiValue.includes(message.content) || message.content.endsWith('quoi ?') || message.content.endsWith('quoi')) && !message.author.bot) {
         message.reply('FEUR');
+    } else if (heinValue.includes(message.content) && !message.author.bot) {
+        message.reply('Deux trois t\'es une oie, quatre cinq six t\'es une saucisse.');
+    } else if (message.content === 'Quelle est le GOTY 2022 ?') {
+        message.reply('Bah Elden Ring évidement.');
+    } else if (random < 0.05 && !message.author.bot) {
+        console.log("héhé");
+        message.reply('https://media.tenor.com/8Ne_GzdBcikAAAAC/les-artisans-rotisseurs-palmashow.gif');
     }
 })
 
